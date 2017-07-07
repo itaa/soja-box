e578dffda51ac5102c1c981660415efdf8d198e4
iccccing@gmail.com <iccccing@gmail.com>
2017-07-07 13:27:31 +0800

    Some test.

--- soja_someTest.py
+++ soja_someTest.py
@@ -2,6 +2,11 @@ from datetime import datetime
 
 
 def how_many_days(other_day='2019-1-1'):
+    """
+    How many days today to other day.
+    :param other_day: some day with string eg.:'2019-1-1'
+    :return:
+    """
     today_date = datetime.now()
     other_day_date = datetime.strptime(other_day, '%Y-%m-%d')
     date_count = other_day_date - today_date
@@ -22,9 +27,32 @@ def factors(integer=30):
     return result
 
 
+def cal_num(*numbers, **kv):
+    if 'kind' in kv:
+        kind_value = kv.get('kind')
+
+    if 'ignore_max' in kv:
+        ignore_max = kv.get('ignore_max')
+        if ignore_max:
+            numbers = list(sorted(numbers))
+            if len(numbers) != 0:
+                _ = numbers.pop(-1)
+    result = 0
+
+    for i in numbers:
+        result += i
+    if kind_value == 'sum':
+        return result
+    if kind_value == 'avg':
+        return result / len(numbers)
+
+
 if __name__ == '__main__':
     print(factors(100))
     print(how_many_days())
+    numbers = [1, 3, 5, 7, 9]
+    kv = {'kind': 'sum', 'ignore_max': False}
 
+    print(cal_num(*numbers, **kv))
 
 
